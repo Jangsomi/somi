01040e60bbbaaf2c77f0783b850230c712f24e3d
CAD Client <CAD Client>
2016-04-01 14:17:18 +0900

    ex05 study drills 실습

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,7 +1,7 @@
 name = 'Zed A. Shaw'
 age = 35 # not a lie
-height = 74 # inches
-weight = 180 # lbs
+height = 187.96 # centimeters
+weight = 81.65 # kg
 eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
