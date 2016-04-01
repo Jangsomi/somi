d03c8429d42c3ceb2ffedae04dfbcba81a8d46d5
CAD Client <CAD Client>
2016-04-01 15:12:49 +0900

    ex05 study drills 실습

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,14 +1,17 @@
-name = 'Zed A. Shaw'
-age = 35 # not a lie
-height_cm = 187.96 # centimeters
-weight_kg = 81.65 # kg
-eyes = 'Blue'
+name = 'Somi Jang'
+age = 22 # not a lie
+height_cm = 171.6 # centimeters
+cm_to_inch = 1.0 / 2.54
+height_inch = height_cm * cm_to_inch
+weight_kg = 52 # kg
+eyes = 'Brown'
 teeth = 'White'
 hair = 'Brown'
-
+# place holder
 print "Let's talk about %s." % name
-print "He's %d centimeters tall." % height_cm
-print "He's %d kilograms heavy." % weight_kg
+print "She's %g centimeters tall." % height_cm
+print "She's %g kilograms tall." % height_inch
+print "She's %d kilograms heavy." % weight_kg
 print "Actually that's not too heavy."
 print "He's got %s eyes and %s hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee." % teeth
