06cfa578723913bcd59ee81b6201aeee40681e85
CAD Client <CAD Client>
2016-04-01 16:06:23 +0900

    ex06 study drills 실습

--- ex06/ex06.py
+++ ex06/ex06.py
@@ -1,20 +1,25 @@
+# set up the number of people
 x = "There are %d types of people." % 10
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
 
-print x
-print y
+#create x, y
+print (x)
+print (y)
 
-print "I said: %r." % x
-print "I also said: '%s'." % y
+#set up x, y
+print ("I said: %r." % x)
+print ("I also said: '%s'." % y)
 
+# set up sentences.
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
 
 print joke_evaluation % hilarious
 
+# multiple w and e
 w = "This is the left side of..."
 e = "a string with a right side."
 
-print w + e
+print (w + e)
