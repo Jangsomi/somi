f1e8681ecf8104ab7383accfbbea9f965959ea3c
CAD Client <CAD Client>
2016-04-15 15:56:18 +0900

    editted ex17.py

--- ex17/ex17.py
+++ ex17/ex17.py
@@ -11,15 +11,15 @@ indata = in_file.read()
 
 print ("The input file is %d bytes long") % len(indata)
 
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
+print ("Does the output file exist? %r") % exists(to_file)
+print ("Ready, hit RETURN to continue, CTRL-C to abort.")
 raw_input()
 # python3 : input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
-print "Alright, all done."
+print ("Alright, all done.")
 
 out_file.close()
 in_file.close()
