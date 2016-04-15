f53a29617fec14f2408b6407317e39cbf32617e8
CAD Client <CAD Client>
2016-04-15 15:17:41 +0900

    editted ex16.py

--- ex17/ex17.py
+++ ex17/ex17.py
@@ -3,7 +3,7 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
+print ("Copying from %s to %s" % (from_file, to_file))
 
 # we could do these two on one line, how?
 in_file = open(from_file)
@@ -14,6 +14,7 @@ print "The input file is %d bytes long" % len(indata)
 print "Does the output file exist? %r" % exists(to_file)
 print "Ready, hit RETURN to continue, CTRL-C to abort."
 raw_input()
+# python3 : input()
 
 out_file = open(to_file, 'w')
 out_file.write(indata)
