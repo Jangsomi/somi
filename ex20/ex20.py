be855fbcb45ffa2a0677d8544a2c04138f10a447
CAD Client <CAD Client>
2016-04-29 15:04:43 +0900

    ex20.py 수정

--- ex20/ex20.py
+++ ex20/ex20.py
@@ -2,14 +2,17 @@ from sys import argv
 
 script, input_file = argv
 
-def print_all(f) :
-    print (f.read())
 
-def rewind(f) :
+def print_all(f):
+    print(f.read())
+
+
+def rewind(f):
     f.seek(0)
 
+
 def print_a_line(line_count, f):
-    print ("%d %s" % (line_count, f.readline()))
+    print("%d %s" % (line_count, f.readline()))
 
 current_file = open(input_file)
 
